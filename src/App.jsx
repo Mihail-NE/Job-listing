869d6795f77077912ef7823e6ca71a490fdd5663
Mihail <harkunovmihail953@gmail.com>
2024-08-24 12:22:05 +0300

    fix positions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import FilterPanel from './components/FilterPanel';
 import JobList from './components/JobList';
 import TheHeader from './components/TheHeader';
+import { useDispatch } from'react-redux';
+import { useEffect } from'react';
+import { addPositions } from './store/positions/position-actions';
+import data from './mock/data.json'
 
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(addPositions(data))
+  })
+
+
   return (
     <>
      <TheHeader />
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,8 @@
 import { createStore } from "redux";
-import rootReducer from "./reducers";
+import { rootReducer } from "./root-reducer";
 
-export const store = createStore({
-  reducer: rootReducer,
-})
+export const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+);
